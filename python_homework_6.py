5d90c53de546f3ae0b4adb041c8a113ef82bbd87
Kanunnikov Sergey <SergeyKanunnikov@MacBook-Pro-Kanunnikov.local>
2017-07-22 19:48:51 +0300

    correct cookbook import

--- python_homework_6.py
+++ python_homework_6.py
@@ -1,27 +1,58 @@
-
 import re
+
+cook_book = {}
 
 
 def import_cookbook():
-    dish_list = []
-    ingredients_list = []
-    dish_number = 0
-    dish_stats = ["ingredient_name", "quantity", "measure"]
-    cook_book = {}
-    ingredients_count = 0
+    dish_list = []  # список блюд
+    ingredients_list = []  # список ингридиентов
+    dish_number = 0  # номер блюда для использования в списке
+    ingredient_stats = ["ingredient_name", "quantity", "measure"]  # свойства ингридиента
+    ingredients_count = 0  # счетчик ингредиентов
+    left_index = 0 # левая граница для перебора списка ингредиентов
+    ingredients_count_list = []
     with open(file="recipes.txt") as f:
         for line in f:
-            if re.match('^[а-яА-Я]\D*$', line):
+            if re.match('^[а-яА-Я]\D*$', line):  # регулярка для поиска названий блюд
                 dish_list.append(line.split(' ', 1)[0].strip())  # создание списка блюд
-                # print(dish_list)
-            if re.match('^\d$', line):
+                left_index += ingredients_count
+            if re.match('^\d$', line):  # регулярка для поиска количества ингредиентов
                 ingredients_count = int(line)
                 dish_number += 1  # номер блюда из списка
-                # print(ingredients_count)
-            if re.match('^[а-яА-Я]*\s\|\s\d*\s\|\s[а-яА-Я]*$', line):
+                ingredients_count_list.append(ingredients_count)
+            if re.match('^[а-яА-Я]*\s\|\s\d*\s\|\s[а-яА-Я]*$', line):  # регулярка для поиска строки со свойствами ингредиента
                 dish_ingredients_list = line.strip().split(" | ")
-                ingredients_list.append(dict(zip(dish_stats, dish_ingredients_list)))  # список словарей ингредиентов
-                cook_book.update({dish_list[dish_number-1]: ingredients_list[0:ingredients_count]})
-    print(cook_book)
+                ingredients_list.append(dict(zip(ingredient_stats, dish_ingredients_list)))  # список словарей ингредиентов
+                cook_book.update({dish_list[dish_number-1]: ingredients_list[left_index:left_index+ingredients_count]})
 
-import_cookbook()
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            new_shop_list_item = dict(ingredient)
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingredient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
+    return shop_list
+
+
+def print_shop_list(shop_list):
+    for shop_list_item in shop_list.values():
+        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+
+
+def create_shop_list():
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print_shop_list(shop_list)
+
+
+def main():
+    import_cookbook()
+    create_shop_list()
+
+main()
